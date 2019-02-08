751651bb45b20dda23bee0c732d76d9e9e75e557
Masa <masa@simasasolutions.com>
2019-02-08 18:36:02 +0200

    Removed deprecated code

--- src/main/java/com/imaginarycode/minecraft/redisbungee/util/IOUtil.java
+++ src/main/java/com/imaginarycode/minecraft/redisbungee/util/IOUtil.java
@@ -1,9 +1,9 @@
 package com.imaginarycode.minecraft.redisbungee.util;
 
-import com.google.common.io.ByteStreams;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
@@ -11,12 +11,17 @@ import java.nio.charset.StandardCharsets;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class IOUtil {
     public static String readInputStreamAsString(InputStream is) {
-        String string;
         try {
-            string = new String(ByteStreams.toByteArray(is), StandardCharsets.UTF_8);
+            try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+                int readLen;
+                final int bufLen = 4 * 0x400;
+                byte[] buf = new byte[bufLen];
+                while ((readLen = is.read(buf, 0, bufLen)) != -1)
+                    out.write(buf, 0, readLen);
+                return new String(out.toByteArray(), StandardCharsets.UTF_8);
+            }
         } catch (IOException e) {
             throw new AssertionError(e);
         }
-        return string;
     }
 }
